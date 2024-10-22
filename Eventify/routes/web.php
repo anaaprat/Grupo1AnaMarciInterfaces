856624869a6399ebb6b6ac4fde2d6cfb7537f050
anaaprat <garrido.anpra22@cadiz.salesianos.edu>
2024-10-22 10:33:59 +0200

    Models, controllers + rutas

--- Eventify/routes/web.php
+++ Eventify/routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\EventAttendeeController;
+use App\Http\Controllers\NotificationController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +20,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Route::resource('categories', CategoryController::class);
+Route::resource('events', EventController::class);
+Route::resource('event-attendees', EventAttendeeController::class);
+Route::resource('notifications', NotificationController::class);
+Route::resource('payments', PaymentController::class);
+Route::resource('reviews', ReviewController::class);
+Route::resource('users', UserController::class);
