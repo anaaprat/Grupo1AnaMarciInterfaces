be3d337d4d7a8747940191524aafde3ac7b967d3
anaaprat <garrido.anpra22@cadiz.salesianos.edu>
2024-10-23 16:35:44 +0200

    borrar cosas q no hacen falta we

--- Eventify/routes/web.php
+++ Eventify/routes/web.php
@@ -1,12 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\EventController;
-use App\Http\Controllers\EventAttendeeController;
-use App\Http\Controllers\NotificationController;
-use App\Http\Controllers\PaymentController;
-use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\UserController;
 
 /*
@@ -24,10 +18,4 @@ use App\Http\Controllers\UserController;
     return view('welcome');
 });*/
 
-Route::resource('categories', CategoryController::class);
-Route::resource('events', EventController::class);
-Route::resource('event-attendees', EventAttendeeController::class);
-Route::resource('notifications', NotificationController::class);
-Route::resource('payments', PaymentController::class);
-Route::resource('reviews', ReviewController::class);
 Route::resource('users', UserController::class);
