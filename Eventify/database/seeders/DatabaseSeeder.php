97339078301649a7be2154a00f4988557d7a5294
anaaprat <garrido.anpra22@cadiz.salesianos.edu>
2024-10-24 17:28:01 +0200

    pa hacer el admin bien jajajaja

--- Eventify/database/seeders/DatabaseSeeder.php
+++ Eventify/database/seeders/DatabaseSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -15,5 +14,13 @@ class DatabaseSeeder extends Seeder
     {
         // Crea 20 usuarios utilizando la factory
         \App\Models\User::factory()->count(20)->create();
+        \App\Models\User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => '12345678',
+            'activated' => 'true',
+            'email_confirmed' => 'true',
+            'role' => 'admin',
+        ]);
     }
 }
