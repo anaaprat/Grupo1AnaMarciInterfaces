ae38897897d381b943d195cc8ba15146f8d770bc
MarcitaBustelo <marcitabuxtelo@gmail.com>
2024-10-27 18:16:54 +0100

    intento de mandar email

---
+++ Eventify/resources/views/users/wait.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Esperando Activación</title>
+</head>
+<body>
+    <h1>Cuenta Confirmada</h1>
+    <p>Tu cuenta ha sido confirmada. Por favor, espera a que un administrador active tu cuenta.</p>
+</body>
+</html>
