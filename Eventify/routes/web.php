938d604106343733c35ee16e4ec02a1d61229d7e
anaaprat <garrido.anpra22@cadiz.salesianos.edu>
2024-11-05 09:36:56 +0100

    prueba commit yo

--- Eventify/routes/web.php
+++ Eventify/routes/web.php
@@ -15,7 +15,6 @@ Route::get('/', function () {
         return redirect()->route('users.dashboard');
     }
     return view('auth.login');
-    
 })->name('login');
 
 Route::get('/users', [UserController::class, 'index'])->name('users.index')->middleware('role:a');
